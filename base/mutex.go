22327401da603646cec1d197e20e9f98258db06d
Rong <daimaldd@gmail.com>
2014-10-29 00:24:44 +0800

    update

--- base/mutex.go
+++ base/mutex.go
@@ -1,58 +1,58 @@
 package base
 
 import (
-	"fmt"
-	"sync"
+    "fmt"
+    "sync"
 )
 
 type MutexLock struct {
-	mutex sync.Mutex
+    mutex sync.Mutex
 }
 
 func NewMutexLock() *MutexLock {
-	m_ := new(MutexLock)
-	return m_
+    return &MutexLock{}
 }
+
 func (m MutexLock) isLockByThisThread() {}
 
 func (m MutexLock) assertLocked() {}
 
 func (m MutexLock) lock() {
-	fmt.Printf("lock Mutex add %p\n", &m)
-	m.mutex.Lock()
+    fmt.Printf("lock Mutex add %p\n", &m)
+    m.mutex.Lock()
 }
 
 func (m MutexLock) unlock() {
-	fmt.Println("unlcok Mutex add %p\n", &m)
-	m.mutex.Unlock()
+    fmt.Println("unlcok Mutex add %p\n", &m)
+    m.mutex.Unlock()
 }
 
 func (m MutexLock) getPThreadMutex() *MutexLock {
-	return &m
+    return &m
 }
 
 ////////////
 type MutexLockGuard struct {
-	mutex *MutexLock
+    mutex *MutexLock
 }
 
 /// too complex .....
 func LockAndUnlock(mutex_ *MutexLock, f func(args ...interface{}) interface{}) interface{} {
-	(*mutex_).lock()
-	defer func() {
-		if e := recover(); e != nil {
-			fmt.Println(e)
-		}
-	}()
-	f()
-	(*mutex_).unlock()
-	return nil
+    (*mutex_).lock()
+    defer func() {
+        if e := recover(); e != nil {
+            fmt.Println(e)
+        }
+    }()
+    f()
+    (*mutex_).unlock()
+    return nil
 }
 
 func (MLG MutexLockGuard) NewMutexLock(mutex *MutexLock) {
-	(*mutex).lock()
+    (*mutex).lock()
 }
 
 func (MLG MutexLockGuard) DeleteMutexLock(mutex *MutexLock) {
-	(*mutex).unlock()
+    (*mutex).unlock()
 }
